0ad294030569c5ba6f2b6e35755823ac2a6ae62a
Zsomborkkszki <144881316+Zsomborkkszki@users.noreply.github.com>
2024-01-28 10:05:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="main.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <h3>Signup</h3>
+    
+   <form action="includes/formhandler.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="password" name="pwd"placeholder="password">
+        <input type="text" name="email"placeholder="E-Mail">
+        <button>Signup</button>
+   </form>
+</body>
+
+</html>
